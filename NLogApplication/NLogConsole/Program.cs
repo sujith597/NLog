83a27ffcb6a62280899676ec257d3e2e21bbc4a6
Sujith Kumar Yadav Pallapati <sujithkumar.pallapati@innRoad.local>
2017-02-17 18:30:16 +0530

    Updated NUNIT solution

--- NLogApplication/NLogConsole/Program.cs
+++ NLogApplication/NLogConsole/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLog;
 
 namespace NLogConsole
 {
@@ -10,7 +11,16 @@ namespace NLogConsole
     {
         static void Main(string[] args)
         {
+            Logger log = LogManager.GetCurrentClassLogger();
+            log.Trace("This is a Trace message");
+            log.Debug("This is a debug message");
+            log.Info("This is information message");
+            log.Warn("This is warning message");
+            log.Error("This is error");
+            log.Fatal("This is fater error message");
 
+            Console.ReadLine();
         }
+
     }
 }
---
+++ NLogApplication/NLogConsole/TextLogger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogConsole
+{
+    public class TextLogger : ILogger
+    {
+        public void LogMessage(string message)
+        {
+            throw new NotImplementedException();
+        }        
+    }
+}
